49ccdc9fe4d351d8ab6a9c0452bc7d9c905852bd
Avensen Nejersian <anejersian@gmail.com>
2022-06-13 10:07:32 -0400

    Update home.svelte

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -320,7 +320,7 @@
 		<div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
 			<div class="border-t border-gray-200 py-8 text-sm text-gray-500 text-center sm:text-left">
 				<span class="block sm:inline">&copy; Gobierno de Konoha.</span>
-				<span class="block sm:inline">Todas las libertades reservadas.</span>
+				<span class="block sm:inline"></span>
 				<span class="block sm:inline"
 					>Made with
 					<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline text-primary" viewBox="0 0 20 20" fill="currentColor">
